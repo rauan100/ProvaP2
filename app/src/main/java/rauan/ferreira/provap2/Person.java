2b1a090696e722a1682f085298c4b1ce74e6ea05
rauan <rauanamigo@hotmail.com>
2020-11-23 21:15:48 -0300

    Adicionar usuário

---
+++ app/src/main/java/rauan/ferreira/provap2/Person.java
@@ -0,0 +1,29 @@
+package rauan.ferreira.provap2;
+
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Person {
+    public String name;
+    public Integer age;
+
+    public Person() {
+
+    }
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("name", name);
+        result.put("age", age);
+
+        return result;
+    }
+}
